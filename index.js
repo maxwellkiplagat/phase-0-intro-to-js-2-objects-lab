fa9345ebb42dffb3d0182f2c17f9127f627da53d
maxwellkiplagat <maxwellkiplagat0@gmail.com>
2025-03-08 11:57:54 +0300

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Write your solution in this file!
+// Initialize the employee object
+const employee = {
+    name: "maxwell",
+    address: "Satelite",
+
+  };
+  function updateEmployeeWithKeyAndValue(employee, key, value) {
+    console.log({ ...employee, [key]: value })
+    return { ...employee, [key]: value }; 
+  }
+   function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value; 
+    return employee;
+  }
+  function deleteFromEmployeeByKey(employee, key) {
+    const newEmployee = { ...employee }; 
+    delete newEmployee[key]; 
+    return newEmployee; 
+  }
+  function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key]; 
+    return employee;
+  }
+    
